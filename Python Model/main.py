7fdd62f9d129f55e21146730dae53ede5ea1c9d6
Jose Lee <84067630+joselclee@users.noreply.github.com>
2023-12-03 16:23:24 -0800

    lol

--- Python Model/main.py
+++ Python Model/main.py
@@ -5,30 +5,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-# mnist = tf.keras.datasets.mnist
+mnist = tf.keras.datasets.mnist
 
-# (x_train, y_train), (x_test, y_test) = mnist.load_data()
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# x_train = tf.keras.utils.normalize(x_train, axis=1)
-# x_test = tf.keras.utils.normalize(x_test, axis=1)
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
 
-# model = tf.keras.models.Sequential()
-# model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
-# model.add(tf.keras.layers.Dense(128, activation='relu'))
-# model.add(tf.keras.layers.Dense(10, activation='softmax'))
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
+model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dense(10, activation='softmax'))
 
-# model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-# model.fit(x_train, y_train, epochs=1)
+model.fit(x_train, y_train, epochs=1)
 
-# model.save('handwritten.model')
+model.save('handwritten.model')
 
 model = tf.keras.models.load_model('handwritten.model')
 
+# image_number = 1
+# while os.path.isfile(f"drawings/drawing{image_number}.png"):
+#     try:
+#         img = cv2.imread(f"drawings/drawing{image_number}.png")[:,:,0]
+#         img = np.invert(np.array([img]))
+#         prediction = model.predict(img)
+#         print(f"This digit is probably a {np.argmax(prediction)}")
+        
+#         plt.imshow(img[0], cmap=plt.cm.binary)
+#         plt.show()
+#     except:
+#         print("Error")
+#     finally:
+#         image_number += 1
+
 image_number = 1
-while os.path.isfile(f"drawings/drawing{image_number}.png"):
+while os.path.isfile(f"drawings2/drawing{image_number}.png"):
     try:
-        img = cv2.imread(f"drawings/drawing{image_number}.png")[:,:,0]
+        img = cv2.imread(f"drawings2/drawing{image_number}.png")[:,:,0]
         img = np.invert(np.array([img]))
         prediction = model.predict(img)
         print(f"This digit is probably a {np.argmax(prediction)}")
@@ -39,8 +54,3 @@ while os.path.isfile(f"drawings/drawing{image_number}.png"):
         print("Error")
     finally:
         image_number += 1
-
-
-
-
-
